7c3de7445e86e02c4456ab121bd6b65f4f791dd3
emagrorrom <373407641@qq.com>
2016-02-25 11:00:59 +0800

    loadOurchasedItem Task Finish

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,1 +1,34 @@
 //TODO: Please write code in this file.
+function loadPurchasedItem(barcodes) {
+  var codes = divideBarcodes(barcodes);
+  var cartCodes = mergeSameBarcode(codes);
+  return cartCodes;
+}
+
+function divideBarcodes(barcodes) {
+  var codes = [];
+  for (var i = 0; i < barcodes.length; i++) {
+    var codeArr = barcodes[i].split('-');
+    if (codeArr.length < 2) {
+      codes.push({barcode:codeArr[0],num:1});
+    }else {
+      codes.push({barcode:codeArr[0],num:parseInt(codeArr[1])});
+    }
+  }
+  return codes;
+}
+
+function mergeSameBarcode(codes) {
+  var cartCodes = [];
+  var count = 1;
+  for (var i = 1; i < codes.length; i++) {
+    if (codes[i].barcode == codes[i-1].barcode) {
+      count ++;
+    } else {
+      cartCodes.push({barcode:codes[i-1].barcode,num:count});
+      count = codes[i].num;
+    }
+  }
+  cartCodes.push({barcode:codes[codes.length - 1].barcode,num:count})
+  return cartCodes;
+}
