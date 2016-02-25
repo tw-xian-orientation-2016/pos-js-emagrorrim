fb982fb1c6f6cfc43bd09508010caf598f3e2a7e
emagrorrom <373407641@qq.com>
2016-02-25 11:22:37 +0800

    test of loadAllInfoIfItem finish

--- pos_v1/test/task-test.js
+++ pos_v1/test/task-test.js
@@ -1,3 +1,4 @@
+//Task loadPurchasedItem
 describe('loadPurchasedItem', function() {
   var allItems;
   var inputs;
@@ -24,6 +25,52 @@ describe('loadPurchasedItem', function() {
       {barcode:'ITEM000001',num:5},
       {barcode:'ITEM000003',num:2},
       {barcode:'ITEM000005',num:3}
+    ];
+
+    expect(output).toEqual(expectObject);
+  });
+});
+
+//Task loadAllInfoOfItem
+describe('loadAllInfoOfItem', function() {
+  var allItems;
+  var inputs;
+
+  beforeEach(function() {
+    inputs = [
+      {barcode:'ITEM000001',num:5},
+      {barcode:'ITEM000003',num:2},
+      {barcode:'ITEM000005',num:3}
+    ];
+
+  });
+
+  it('should output correct array', function() {
+
+    var output = loadAllInfoOfItem(inputs);
+
+    var expectObject = [
+      {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00,
+        num: 5
+      },
+      {
+        barcode: 'ITEM000003',
+        name: '荔枝',
+        unit: '斤',
+        price: 15.00,
+        num: 2
+      },
+      {
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50,
+        num: 3
+      }
     ];
 
     expect(output).toEqual(expectObject);
